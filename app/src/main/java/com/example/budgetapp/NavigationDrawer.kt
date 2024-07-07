9cd193454621e3383b3b67167819b81fa9efe2e3
pelokbal <pelokbalazs@gmail.com>
2024-07-07 16:16:58 +0300

    feat: add styling for navigation buttons

--- app/src/main/java/com/example/budgetapp/NavigationDrawer.kt
+++ app/src/main/java/com/example/budgetapp/NavigationDrawer.kt
@@ -2,16 +2,19 @@ package com.example.budgetapp
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
@@ -21,12 +24,15 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.wear.compose.material.ContentAlpha
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -48,12 +54,26 @@ fun DrawerButton(
     drawerState: DrawerState,
     scope: CoroutineScope
 ) {
-    Button(onClick = {
-        navController.navigate(destination)
-        if (drawerState.isOpen) scope.launch {
-            drawerState.close()
-        }
-    }) {
+    val start = navController.currentBackStackEntryAsState().value?.destination?.route
+
+    val startIsDestination = start == destination
+    val buttonColors = if (startIsDestination) {
+        ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary)
+    } else {
+        ButtonDefaults.buttonColors(MaterialTheme.colorScheme.onSurface.copy(alpha = ContentAlpha.medium))
+    }
+
+    Button(
+        onClick = {
+            navController.navigate(destination)
+            if (drawerState.isOpen) scope.launch {
+                drawerState.close()
+            }
+        },
+        modifier = Modifier.fillMaxWidth(),
+        shape = RectangleShape,
+        colors = buttonColors
+    ) {
         Text(text)
     }
 }
